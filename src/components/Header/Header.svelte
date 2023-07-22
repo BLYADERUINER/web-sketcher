797e752a45581eca075c6b9d7ec61fd902fea42d
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-07-23 00:47:31 +0400

    added: header component + logo

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Logo from './Logo/Logo.svelte';
+  import Text from './Text/Text.svelte';
+</script>
+
+<header class="header">
+    <Logo />
+    <Text />
+</header>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+</style>
---
+++ src/components/Header/Logo/Logo.svelte
@@ -0,0 +1,14 @@
+<script>
+  import src from '../../../assets/logo.png';
+</script>
+
+<div class="header__container-logo">
+  <img class="header__logo" {src}  alt="Логотип Web-Sketcher" />
+</div>
+
+<style lang="scss">
+  .header__logo {
+    width: 120px;
+    height: 150px;
+  }
+</style>
