aadfd75ae5c2cfe4ab790bb0004dcdca951bfb18
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2024-04-28 00:25:29 +0400

    added: pages meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,17 @@
   import CanvasSetting from '$lib/components/CanvasSetting/index.svelte';
 </script>
 
+<svelte:head>
+  <title>Web-Sketcher</title>
+  <meta name="description" content="Web drawing tool" />
+  <meta name="keywords" content="web-sketcher, рисование в интернете, рисовалка, web drawing tool" />
+  <meta property="og:title" content="Web-Sketcher" />
+  <meta property="og:type" content="website" />
+  <meta property="og:description" content="Web drawing tool" />
+  <meta property="og:images" content="https://web-sketcher-nu.vercel.app/logo.png" />
+  <meta property="og:url" content="https://web-sketcher-nu.vercel.app/" />
+</svelte:head>
+
 {#if $storeCanvasSetting.canvas_visible}
   <Canvas /> 
   {:else}
